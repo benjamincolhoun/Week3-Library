0c4c30fbca433edbfb6bf748674fbaabdb43e034
benjamincolhoun <colhoun-b2@ulster.ac.uk+>
2024-10-08 10:45:42 +0100

    finished code

---
+++ Week3-Library/Member.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } 
+        }
+        public string Name
+        {
+            get { return name; }  
+            set { name = value; } 
+        }
+        public string Address
+        {
+            get { return address; }  
+            set { address = value; } 
+        }
+        public int Phone
+        {
+            get { return phone; } 
+            set { phone = value; } 
+        }
+
+        
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.memberId = memberId;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"memberId: {memberId}");
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"address: {address}");
+            Console.WriteLine($"phone: {phone}");
+            Console.WriteLine();
+        }
+    }
+}
--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -7,11 +7,20 @@ class Program
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
         Book book1 = new Book("C# Methods and classes", "Microsoft","55667778");
 
-        
-
-        
         book.DisplayInfo();
         book1.DisplayInfo();
+
+        Console.WriteLine("Currently available books");
+        book.DisplayInfo();
+        book1.DisplayInfo();
+
+
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
 
     }
 }
