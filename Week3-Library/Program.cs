330bfc53f8fff8ac4df4977a3d01033310504db9
benjamincolhoun <colhoun-b2@ulster.ac.uk+>
2024-10-08 09:43:12 +0100

    re commit

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(string[] args)
+    {
+        
+        Book book = new Book();
+
+        
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        
+        Book book1 = new Book();
+        book1.Title = "C# Methods and classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+        
+        Console.WriteLine($"Book title: {book.Title}");
+        Console.WriteLine($"Book Author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+        Console.WriteLine();
+
+        
+        Console.WriteLine($"Book title: {book1.Title}");
+        Console.WriteLine($"Book Author: {book1.Author}");
+        Console.WriteLine($"Book ISBN: {book1.ISBN}");
+    }
+}
